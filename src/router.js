3cbbf3e392276757aa5e0a2acb94362a8dc5ce46
José Alves Feitosa Neto <netoofeitosa@gmail.com>
2024-06-03 22:52:37 -0300

    configurações gerais

--- src/router.js
+++ src/router.js
@@ -4,6 +4,9 @@ const aprovacoesController = require("./controllers/aprovacoesControllers");
 
 const router = express.Router();
 
+router.get("/", (_req, res) => {
+  res.status(200).json({ message: "Server OK" });
+});
 router.get("/users", usersControllers.getAll);
 router.post("/users", usersControllers.createUser);
 router.post("/login", usersControllers.login);
